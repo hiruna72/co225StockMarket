1be44a18d46050c04ad52dc64abb0fe0eb0e9f1d
Hiruna Samarakoon <hiruna72@gmail.com>
2019-10-15 15:24:52 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Hello World"
 wget -O samtools.tar.gz "https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2" && tar -xvf samtools.tar.gz && cd samtools-1.9/
+./configure CC="gcc -static" --without-curses
+ make
