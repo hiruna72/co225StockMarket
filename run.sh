027b6e8b47fcc826f8e76dbb2693f450ace9565c
Hiruna Samarakoon <hiruna72@gmail.com>
2019-10-15 15:13:42 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hello World"
+wget -O samtools.tar.gz "https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2" && tar -xvf samtools.tar.gz && cd samtools-1.9/
